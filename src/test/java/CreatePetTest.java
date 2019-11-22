94338fccd47303e999339e09fa5b986c7549c242
Yuliia Pikhterova <juliaokopna@gmail.com>
2019-11-22 12:10:06 +0200

    lesson 6

--- src/test/java/CreatePetTest.java
+++ src/test/java/CreatePetTest.java
@@ -1,15 +1,22 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
 
 import static org.hamcrest.Matchers.anyOf;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.core.Is.is;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class CreatePetTest {
 
+    static long petId;
+
     @Test
-    public void createPet() {
+    public void test1CreatePet() {
 
         String body = "{\n" +
                 "  \"id\": 0,\n" +
@@ -30,7 +37,7 @@ public class CreatePetTest {
                 "  \"status\": \"available\"\n" +
                 "}";
 
-        RestAssured
+        ValidatableResponse response = RestAssured
                 .given()
                 .body(body)
                 //.header("ContentType", "application/json")
@@ -44,5 +51,40 @@ public class CreatePetTest {
                 .body("category.name", is (not ("")))
                 .log().all();
 
+        petId =  response.extract().path("id");
+        //response.extract().body().jsonPath().getString("id");
+        System.out.println(petId);
+
     }
+
+
+    @Test
+    public void test2GetPetById() {
+        System.out.println(petId);
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .get("https://petstore.swagger.io/v2/pet/"+petId)
+                .then()
+                .statusCode(anyOf(is(200),is(201)))
+                .body("category.name", is (not ("")))
+                .log().all();
+
+    }
+
+    @Test
+    public void test3DeletePetById() {
+        System.out.println(petId);
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .log().all()
+                .delete("https://petstore.swagger.io/v2/pet/"+petId)
+                .then()
+                .statusCode(anyOf(is(200),is(201)))
+                .log().all();
+
+    }
+
 }
